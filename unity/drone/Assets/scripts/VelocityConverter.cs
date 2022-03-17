241085941061423fb002595a6a0e80165ebbb782
thedylone <thedylone@gmail.com>
2022-03-17 18:00:13 +0800

    draft velocity converter

--- unity/drone/Assets/scripts/VelocityConverter.cs
+++ unity/drone/Assets/scripts/VelocityConverter.cs
@@ -7,9 +7,8 @@ using UnityEngine;
 public class VelocityConverter : MonoBehaviour
 {
     public float MaxSpeed = 15f;
-    public float MaxTiltAngle = 25f;
+    public float MaxTiltDeg = 25f;
     public float tiltSpeed = 1;
-    public float Responsiveness = 25;
     [Range(-1, 1)]
     public float vx = 0.5f;
     [Range(-1, 1)]
@@ -17,27 +16,45 @@ public class VelocityConverter : MonoBehaviour
 
     private Rigidbody rb;
     private float mass;
-    private float drag = 0.0280f;
-    private float thrust;
+    private float drag;
+
+    void Update()
+    {
+        Convert(vx, vy);
+    }
 
     public void Convert(float vx, float vy)
     {
         rb = GetComponent<Rigidbody>();
         mass = rb.mass;
         rb.drag = 0;
+        drag = mass * 9.81f * Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) / (MaxSpeed * MaxSpeed);
         float ux = rb.velocity.x;
         float uy = rb.velocity.z;
         // prevent vx and vy from exceeding -1 to 1
         vx = Mathf.Min(Mathf.Abs(vx), 1) * Mathf.Sign(vx);
         vy = Mathf.Min(Mathf.Abs(vy), 1) * Mathf.Sign(vy);
 
-        float tiltx = Mathf.Atan(Mathf.Tan(MaxTiltAngle * Mathf.PI / 180) * vx * vx) * Mathf.Sign(vx);
-        float tilty = Mathf.Atan(Mathf.Tan(MaxTiltAngle * Mathf.PI / 180) * vy * vy) * Mathf.Sign(vy);
+        // float trueTiltx = Mathf.Atan(Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) * vx * vx) * Mathf.Sign(vx);
+        float trueTiltx = Mathf.Atan(Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) * vx * vx);
+        // float trueTilty = Mathf.Atan(Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) * vy * vy) * Mathf.Sign(vy);
+        float trueTilty = Mathf.Atan(Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) * vy * vy);
+
+        float currentTiltx = transform.rotation.eulerAngles.z;
+        float currentTilty = transform.rotation.eulerAngles.x;
+
+        float dx = vx * MaxSpeed - ux;
+        float dy = vy * MaxSpeed - uy;
+
+        float tiltx = Mathf.Min(MaxTiltDeg, Mathf.Abs(Mathf.Atan(Mathf.Tan(MaxTiltDeg * Mathf.PI / 180) * dx * dx) * 180 / Mathf.PI)) * Mathf.Sign(dx);
+        float tilty = Mathf.Min(MaxTiltDeg, Mathf.Abs(Mathf.Atan(dy * dy * drag / (9.81f * mass)) * 180 / Mathf.PI)) * Mathf.Sign(dy);
+        
         // rb.MoveRotation(Quaternion.Euler(new Vector3(0, 0, 0)));
         // transform.Rotate(tilty * 180 / Mathf.PI, 0, tiltx * 180 / Mathf.PI, Space.Self);
-        rb.rotation = Quaternion.Euler(new Vector3(tilty * 180 / Mathf.PI, 0, -tiltx * 180 / Mathf.PI));
-        float fx = mass * 9.81f * Mathf.Tan(Mathf.Abs(tiltx)) * Mathf.Sign(tiltx) - drag * ux * ux * Mathf.Sign(ux);
-        float fy = mass * 9.81f * Mathf.Tan(Mathf.Abs(tilty)) * Mathf.Sign(tilty) - drag * uy * uy * Mathf.Sign(uy);
-        rb.AddForce(fx * Responsiveness, 0, fy * Responsiveness);
+        rb.rotation = Quaternion.Euler(new Vector3(tilty, 0, -tiltx));
+        
+        float fx = mass * 9.81f * Mathf.Tan(-currentTiltx * Mathf.PI / 180) - drag * ux * ux * Mathf.Sign(ux);
+        float fy = mass * 9.81f * Mathf.Tan(currentTilty * Mathf.PI / 180) - drag * uy * uy * Mathf.Sign(uy);
+        rb.AddForce(fx, 0, fy);
     }
 }
