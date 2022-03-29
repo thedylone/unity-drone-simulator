0db630a0c8450d3b3c0279b4d9451bc8354e4446
thedylone <thedylone@gmail.com>
2022-03-29 18:07:32 +0800

    synthetic data gen

---
+++ unity/drone/Assets/scripts/Synthetic Data/Randomizer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour
+{
+    public bool RandomPosition;
+    public float MinPosX;
+    public float MaxPosX;
+    public float MinPosY;
+    public float MaxPosY;
+    public float MinPosZ;
+    public float MaxPosZ;
+    public bool RandomRotation;
+    public float MinRotX;
+    public float MaxRotX;
+    public float MinRotY;
+    public float MaxRotY;
+    public float MinRotZ;
+    public float MaxRotZ;
+
+    public void Randomize()
+    {
+        if (RandomPosition)
+        {
+            var pos = new Vector3(Random.Range(MinPosX, MaxPosX), Random.Range(MinPosY, MaxPosY), Random.Range(MinPosZ, MaxPosZ));
+            transform.localPosition = pos;
+        }
+        if (RandomRotation)
+        {
+            var rot = new Vector3(Random.Range(MinRotX, MaxRotX), Random.Range(MinRotY, MaxRotY), Random.Range(MinRotZ, MaxRotZ));
+            transform.localRotation = Quaternion.Euler(rot);
+        }
+    }
+}
