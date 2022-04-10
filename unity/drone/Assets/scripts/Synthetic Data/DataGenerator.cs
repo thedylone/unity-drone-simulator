11ab465beb5d43164fb2b538a88a0cf19c388ea1
thedylone <thedylone@gmail.com>
2022-04-10 20:18:19 +0800

    ui

--- unity/drone/Assets/scripts/Synthetic Data/DataGenerator.cs
+++ unity/drone/Assets/scripts/Synthetic Data/DataGenerator.cs
@@ -131,10 +131,10 @@ public class DataGenerator : MonoBehaviour
         byte[] bytes = image.EncodeToJPG(); // change to .EncodeToPNG() for png files
         Destroy(image);
 
-        File.WriteAllBytes(SavePath + "/images/" + fileCounter + ".jpg", bytes); // change to .png for png files
+        File.WriteAllBytes(SavePath + "/" + fileCounter + ".jpg", bytes); // change to .png for png files
 
         // outputs position data in specific format
-        File.WriteAllText(SavePath + "/labels/" + fileCounter + ".txt", "drone 0.0 0 0.0 " + minX + " " + (screenHeight - maxY) + " " + maxX + " " + (screenHeight - minY) + " 0.0 0.0 0.0 0.0 0.0 0.0 0.0");
+        File.WriteAllText(SavePath + "/" + fileCounter + ".txt", "drone 0.0 0 0.0 " + minX + " " + (screenHeight - maxY) + " " + maxX + " " + (screenHeight - minY) + " 0.0 0.0 0.0 0.0 0.0 0.0 0.0");
 
         fileCounter++;
     }
